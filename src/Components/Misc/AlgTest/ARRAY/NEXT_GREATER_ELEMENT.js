2580f7c3afe3b0b45dcddfd70006c37c877284d7
tanveeralikhan <50921612+tanveeralikhan@users.noreply.github.com>
2024-02-16 11:19:00 +0530

    Update NEXT_GREATER_ELEMENT.js

--- src/Components/Misc/AlgTest/ARRAY/NEXT_GREATER_ELEMENT.js
+++ src/Components/Misc/AlgTest/ARRAY/NEXT_GREATER_ELEMENT.js
@@ -1,5 +1,18 @@
 export const nextGreaterElement = () => {
-  const elements = [7, 12, 1, 20];
-  const addElements = [];
-  for (let i = 0; i++; i < elements.length) {}
+  const nums1 = [4,1,2], nums2 = [1,3,4,2];
+ let finalArray=[];
+    for(let i=0;i<nums1.length;i++){
+         let indexNums1=nums2.indexOf(nums1[i])+1;
+        console.log(indexNums1);
+        for(let j=indexNums1;j<nums2.length;j++){
+        	if(nums2[j]>nums1[i]){
+          	finalArray.push(nums2[j]);
+            break;
+          }
+        }
+        if(!finalArray[i]){
+        	finalArray.push(-1);
+        }
+    }
+    console.log(finalArray)
 };
